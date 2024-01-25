34bebfc3476732a0d4416b194e15fe577675e221
inclined4X <jezemiahsam48@gmail.com>
2024-01-25 23:03:43 +0000

    finished objects Methods

--- script.js
+++ script.js
@@ -303,4 +303,50 @@ console.log(jonas);
 
 console.log(`${jonas.firstName} has ${jonas.friends.length} friends and his bestfriend is called ${jonas.friends[0]}`)
 */
-git 
+
+
+const jonas = {
+    firstName: 'Jonas',
+    lastName: 'Schmedtmann',
+    birthYear: 1991,
+    job: 'teacher',
+    friends: ['Michael', 'Peter', 'Steven'],
+    hasDriversLicense: true,
+
+    // calcAge: function(birthYear){
+    //     return 2037 - birthYear
+    // }
+
+
+    // calcAge: function(){
+    //     // console.log(this)
+    //     return 2037 - this.birthYear;
+    // }
+
+
+    calcAge: function(){
+        this.age = 2037 - this.birthYear
+        return this.age;
+    },
+
+
+    getSummary: function(){
+        return `${this.firstName} is a ${this.calcAge()}-year old ${this.job}, and he has a ${this.hasDriversLicense ? 'a' : 'no'} driver's license`
+    }
+};
+
+// console.log(jonas)
+
+
+// console.log(jonas.calcAge());
+
+
+// console.log(jonas.age);
+// console.log(jonas.age);
+// console.log(jonas.age);
+
+
+
+
+// console.log(jonas["calcAge"](1991));
+console.log(jonas.getSummary());
